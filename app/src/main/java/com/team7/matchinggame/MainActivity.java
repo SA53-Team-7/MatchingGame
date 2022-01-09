876179b904b1b36968b41a1ade301fc6609c0d73
Howard0910 <bojiun5489@gmail.com>
2022-01-09 18:42:00 +0800

    Add splash screen and modify home and setting page

--- app/src/main/java/com/team7/matchinggame/MainActivity.java
+++ app/src/main/java/com/team7/matchinggame/MainActivity.java
@@ -2,15 +2,10 @@ package com.team7.matchinggame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
