d5fa55def1772ff60cac740ea0d4537b4946ab21
Howard0910 <bojiun5489@gmail.com>
2022-01-09 13:30:29 +0800

    Add home and setting page

--- app/src/main/java/com/team7/matchinggame/MainActivity.java
+++ app/src/main/java/com/team7/matchinggame/MainActivity.java
@@ -2,17 +2,45 @@ package com.team7.matchinggame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+    implements View.OnClickListener{
+    Button btn1;
+    Button btn2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //        Intent intent = new Intent(this, FetchImage.class);
-        //        startActivity(intent);
+        btn1 = findViewById(R.id.startGame);
+        btn1.setOnClickListener(this);
+
+        btn2 = findViewById(R.id.settings);
+        btn2.setOnClickListener(this);
+
+    }
+
+
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+
+        if(id == R.id.startGame){
+            Intent intent = new Intent(this,FetchImage.class);
+            startActivity(intent);
+        }
+
+        else if(id == R.id.settings){
+            Intent intent = new Intent(this,Settings.class);
+            startActivity(intent);
+        }
     }
 }
