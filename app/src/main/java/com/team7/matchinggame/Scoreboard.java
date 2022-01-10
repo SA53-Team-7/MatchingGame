bf1af7eab40f4f3440ccad7af91a387c3376bc03
Lina <82659876+Wanderstarz@users.noreply.github.com>
2022-01-10 15:54:06 +0800

    Added scoreboard

---
+++ app/src/main/java/com/team7/matchinggame/Scoreboard.java
@@ -0,0 +1,17 @@
+package com.team7.matchinggame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+public class Scoreboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scoreboard);
+
+    }
+
+}
