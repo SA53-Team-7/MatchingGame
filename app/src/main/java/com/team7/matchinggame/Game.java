7c1b21186cad961db0cf617d43e08dbcc8f1799e
frozenpotatoes <52646745+si-thu-zaw@users.noreply.github.com>
2022-01-12 11:23:39 +0800

    Fixed clickable matched images, some refactoring, optimisation and comments
    Changed all ImageView to ImageButton, fixed typos, removed redundant code

--- app/src/main/java/com/team7/matchinggame/Game.java
+++ app/src/main/java/com/team7/matchinggame/Game.java
@@ -11,7 +11,6 @@ import android.os.Handler;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 
@@ -23,16 +22,16 @@ import java.util.Locale;
 
 public class Game extends AppCompatActivity implements View.OnClickListener{
     private Context context;
-	private ArrayList<ImageView> ImageViewList = new ArrayList<>();
-	private String[] assignedImages = new String[12];
-	private Integer[] gridStatus = new Integer[12];
-	private File[] allImages = new File[6];
-	private Integer seconds = 0;
-	private Integer gameProgress = 0;
-	private boolean stopwatchStatus = true;
-	private boolean isbusy = false;
-	private String selected1;
-	private View previousGrid;
+	private final ArrayList<ImageButton> ImageButtonList = new ArrayList<>(); //contains all imagebuttons
+	private final String[] assignedImages = new String[12];	//contains images randomly assigned to imagebuttons, in same index
+	private Integer[] gridStatus = new Integer[12];		//contains status of the imagebuttons 0 = hidden, 1 = revealed, 2 = revealed, matched and locked
+	private File[] allImages = new File[6];				//contains the 6 .jpg files selected by the user
+	private Integer seconds = 0;						//stores the time taken by the user
+	private Integer gameProgress = 0;					//stores the number of successful matches
+	private boolean stopwatchStatus = true;				//flag for starting/stopping the stopwatch
+	private boolean isBusy = false;						//flag for blocking UI inputs during delays
+	private String selected1;							//stores the image file location of the first selection match
+	private View previousGrid;							//stores the imagebutton of the first selection match
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,18 +39,18 @@ public class Game extends AppCompatActivity implements View.OnClickListener{
         setContentView(R.layout.activity_game);
 		context = this;
         retrieveImages();
-        getAllImageViews();
+        getAllImageButtons();
         assignImagesToGrid();
-		isbusy = true;
+		isBusy = true;
 		Handler handler = new Handler();
 		handler.postDelayed(new Runnable() {
 			@Override
 			public void run() {
 				hideImages();
 				runStopwatch();
-				isbusy = false;
+				isBusy = false;
 			}
-		}, 3000);
+		}, 1500);
 
 		//test stuff
 		Button button = findViewById(R.id.testSkip);
@@ -60,19 +59,21 @@ public class Game extends AppCompatActivity implements View.OnClickListener{
 
     protected void retrieveImages(){
 		File dir = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+		//retrieve all .jpg files in the SDcard pictures directory into allImages[]
 		allImages = dir.listFiles(new FilenameFilter() {
 			public boolean accept(File dir, String name) {
 				return (name.endsWith(".jpg"));
 			}
 		});
 	}
-	protected void getAllImageViews() {
+	protected void getAllImageButtons() {
+    	//retrieve all imagebuttons into ImageButtonList<>
 		for (int i = 1; i <= 12; i++) {
 			try {
 				int imgViewId = R.id.class.getField("img" + i).getInt(null);
-				ImageView imgView = findViewById(imgViewId);
+				ImageButton imgView = findViewById(imgViewId);
 				imgView.setOnClickListener(this);
-				ImageViewList.add(imgView);
+				ImageButtonList.add(imgView);
 			} catch (IllegalAccessException e) {
 				e.printStackTrace();
 			} catch (NoSuchFieldException e) {
@@ -82,31 +83,27 @@ public class Game extends AppCompatActivity implements View.OnClickListener{
 	}
 
 	protected void assignImagesToGrid() {
+    	//array to track which the number of times the images have been assigned
     	int[] count = new int[allImages.length];
 		Arrays.fill(count, 0);
-		for (int i = 0; i < ImageViewList.size(); i++){
-			int random = 0;
+		int random;
+		for (int i = 0; i < ImageButtonList.size(); i++){
 			do {
+				//pick a random image to assign
 				random = (int) (Math.random() * allImages.length);
-			}while (count[random] > 1);
+			}while (count[random] > 1);  //repeat until an image picked has been assigned less than 2 times
 			assignedImages[i] = allImages[random].getAbsolutePath();
-			ImageViewList.get(i).setImageBitmap(BitmapFactory.decodeFile(allImages[random].getAbsolutePath()));
+			//show assigned images on grid
+			ImageButtonList.get(i).setImageBitmap(BitmapFactory.decodeFile(allImages[random].getAbsolutePath()));
 			count[random]+=1;
 		}
 		Arrays.fill(gridStatus, 0);
 	}
 
 	protected void hideImages() {
-		for (int i = 1; i <= 12; i++) {
-			try {
-				int imgViewId = R.id.class.getField("img" + i).getInt(null);
-				ImageView imgView = findViewById(imgViewId);
-				imgView.setImageResource(R.drawable.question_mark);
-			} catch (IllegalAccessException e) {
-				e.printStackTrace();
-			} catch (NoSuchFieldException e) {
-				e.printStackTrace();
-			}
+    	//replace all shown images with question mark drawable
+		for (ImageButton btn: ImageButtonList) {
+			btn.setImageResource(R.drawable.question_mark);
 		}
 		TextView head = findViewById(R.id.gameHead);
 		head.setText("Match!");
@@ -114,14 +111,17 @@ public class Game extends AppCompatActivity implements View.OnClickListener{
 
 	protected void matchImage(View view) {
 		int grid = getGridNumber(view);
+		//if nothing has previously been selected
 		if (selected1 == null) {
 			selected1 = assignedImages[grid];
 			previousGrid = view;
 			toggleImage(grid, view);
 			return;
 		}
+		//if there is a previous selection for match
 		if (selected1 != null) {
-			if (selected1 == assignedImages[grid]){
+			//if the 2 selections match
+			if (selected1.equals(assignedImages[grid])){
 				selected1 = null;
 				addScore();
 				toggleImage(grid, view);
@@ -132,8 +132,9 @@ public class Game extends AppCompatActivity implements View.OnClickListener{
 				MediaPlayer matched = MediaPlayer.create(Game.this, R.raw.match);
 				matched.start();
 			}
+			//if selections don't match
 			else {
-				isbusy = true;
+				isBusy = true;
 				toggleImage(grid, view);
 				Handler handler = new Handler();
 				handler.postDelayed(new Runnable() {
@@ -143,7 +144,7 @@ public class Game extends AppCompatActivity implements View.OnClickListener{
 						toggleImage(grid, view);
 						previousGrid = null;
 						selected1 = null;
-						isbusy = false;
+						isBusy = false;
 					}
 				}, 500);
 			}
@@ -152,48 +153,44 @@ public class Game extends AppCompatActivity implements View.OnClickListener{
 
 	@Override
 	public void onClick(View view) {
-    	if (isbusy) {
-    		return;
-		}
-		else {
+    	//block inputs if isBusy flag is true
+		if (!isBusy) {
 			int id = view.getId();
 			if (id == R.id.testSkip) {
 				addScore();
 			}
 			else{
-				if (previousGrid != null) {
-					if (id != previousGrid.getId()){
-						matchImage(view);
-						return;
+				//block input if the grid status is 2 (locked)
+				if (gridStatus[getGridNumber(view)] != 2) {
+					if (previousGrid != null) {
+						//block clicking the already selected grid
+						if (id != previousGrid.getId()){
+							matchImage(view);
+						}
 					}
 					else {
-						return;
+						matchImage(view);
 					}
-				}
-				else {
-					matchImage(view);
-					return;
 				}
 			}
 		}
 	}
 
 	protected void toggleImage(int grid, View view) {
-    	//Integer grid = Integer.parseInt(String.valueOf(id.charAt(id.length()-1)));
-		ImageView imageView = findViewById(view.getId());
+		ImageButton imageButton = findViewById(view.getId());
+		//hide image if status is 1 (revealed)
     	if (gridStatus[grid] == 1) {
-			imageView.setImageResource(R.drawable.question_mark);
+			imageButton.setImageResource(R.drawable.question_mark);
 			gridStatus[grid] = 0;
-
+		//show image if status is 0 (hidden), ignore if status is 2 (locked)
 		} else if (gridStatus[grid] == 0) {
-			imageView.setImageBitmap(BitmapFactory.decodeFile(assignedImages[grid]));
+			imageButton.setImageBitmap(BitmapFactory.decodeFile(assignedImages[grid]));
 			gridStatus[grid] = 1;
-		} else {
-    		return;
 		}
 	}
 
 	protected void runStopwatch() {
+    	//run every second to add 1 to the int 'seconds' and update textView textGameTimer
 		TextView timeDisplay = findViewById(R.id.textGameTimer);
 		Handler handler = new Handler();
 		handler.post(new Runnable() {
@@ -204,7 +201,7 @@ public class Game extends AppCompatActivity implements View.OnClickListener{
 				int secs = seconds % 60;
 				String time = String.format(Locale.getDefault(),"%d:%02d:%02d", hours, minutes, secs);
 				timeDisplay.setText(time);
-				if (stopwatchStatus == true) {
+				if (stopwatchStatus) {
 					seconds++;
 				}
 				handler.postDelayed(this, 1000);
@@ -213,12 +210,13 @@ public class Game extends AppCompatActivity implements View.OnClickListener{
 	}
 
 	protected void addScore() {
+    	//add 1 to the game progress for each match, ends calls endGame after all matches are complete (6)
     	gameProgress++;
 		TextView progress = findViewById(R.id.textGameProgress);
 		progress.setText(gameProgress + " out of 6 Matches");
 		if (gameProgress == 6) {
 			stopwatchStatus = false;
-			isbusy = true;
+			isBusy = true;
 			Handler handler = new Handler();
 			handler.postDelayed(new Runnable() {
 				@Override
@@ -230,14 +228,16 @@ public class Game extends AppCompatActivity implements View.OnClickListener{
 	}
 
 	protected void endGame() {
+    	//calls EndGame activity, passes the user's time
 		Intent intent = new Intent(this, EndGame.class);
 		intent.putExtra("timeElapsed", seconds);
 		startActivity(intent);
 	}
 
+
 	protected Integer getGridNumber(View view) {
+		//returns the grid number (1-12) from the view id
 		String idString = view.getResources().getResourceName(view.getId());
-		Integer grid = Integer.parseInt(idString.substring(29))-1;
-		return grid;
+		return Integer.parseInt(idString.substring(29))-1;
 	}
 }
